e7be0faf1200f8bffaf58f4bb2dcd72290a00e95
hyochan <dooboolab@gmail.com>
2018-09-11 04:50:26 +0900

    Changed example app title.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'dooboolab flutter calendar',
       theme: new ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter Calendar Carousel Exmaple'),
     );
   }
 }
