79b15c5a6ccd6fc77e39eede4bcc988f29de9bd7
Edgar <ed@appsynth.net>
2019-02-15 11:18:10 +0700

    define header variables

--- lib/src/calendar_header.dart
+++ lib/src/calendar_header.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 import 'default_styles.dart';
 
 class CalendarHeader extends StatelessWidget {
+	CalendarHeader({
+		this.showHeader,
+		this.headerStyle,
+		this.showHeaderButtons,
+		this.headerIconColor,
+		this.onLeftButtonPressed,
+		this.onRightButtonPressed,
+		this.isTitleTouchable,
+		this.onHeaderTitlePressed
+	});
+	final bool showHeader;
+	final TextStyle headerStyle;
+	final bool showHeaderButtons;
+	final Color headerIconColor;
+	final VoidCallback onLeftButtonPressed;
+	final VoidCallback onRightButtonPressed;
+	final bool isTitleTouchable;
+	final VoidCallback onHeaderTitlePressed;
 
 	@override
   Widget build(BuildContext context) {
