9fc6bb055718d8c7300884fd396c7dd4c2e07cc0
edgar <edgrr.trem000@gmail.com>
2019-02-27 14:43:41 +0700

    change wrapped, text short weekdays

--- test/src/weekday_row_test.dart
+++ test/src/weekday_row_test.dart
@@ -1,26 +1,34 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show WeekdayFormat;
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' show DateFormat;
 
 import 'package:flutter_calendar_carousel/src/weekday_row.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final format = DateFormat.yMMM("en");
+  final format = DateFormat.yMMM("en_US");
   final margin = const EdgeInsets.only(bottom: 4.0);
 
-  testWidgets('test weekday row', (WidgetTester tester) async {
+  testWidgets('test short weekday row', (WidgetTester tester) async {
     await tester.pumpWidget(
         wrapped(
           WeekdayRow(true,
               WeekdayFormat.short,
               margin, null, format),
-        ));
+        )
+    );
+
+    expect(find.text('Sun'), findsOneWidget);
+    expect(find.text('Mon'), findsOneWidget);
+    expect(find.text('Tue'), findsOneWidget);
+    expect(find.text('Wed'), findsOneWidget);
+    expect(find.text('Thu'), findsOneWidget);
+    expect(find.text('Fri'), findsOneWidget);
+    expect(find.text('Sat'), findsOneWidget);
   });
 }
 
-Widget wrapped(Widget widget) => MaterialApp(
-  home: Container(
-    child: Material(child: widget),
-  ),
+Widget wrapped(Widget widget) => Directionality(
+  textDirection: TextDirection.ltr,
+  child: widget,
 );
