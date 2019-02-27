0cb0ed6b0b214a799093c4cb92b322b5eb670353
edgar <edgrr.trem000@gmail.com>
2019-02-27 15:04:15 +0700

    test different usecases

--- test/src/weekday_row_test.dart
+++ test/src/weekday_row_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show WeekdayFormat;
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show WeekdayFormat;
 import 'package:intl/intl.dart' show DateFormat;
 
 import 'package:flutter_calendar_carousel/src/weekday_row.dart';
@@ -10,13 +11,52 @@ void main() {
   final margin = const EdgeInsets.only(bottom: 4.0);
 
   testWidgets('test short weekday row', (WidgetTester tester) async {
-    await tester.pumpWidget(
-        wrapped(
-          WeekdayRow(true,
-              WeekdayFormat.short,
-              margin, null, format),
-        )
-    );
+    await tester.pumpWidget(wrapped(
+      WeekdayRow(true, WeekdayFormat.short, margin, null, format),
+    ));
+
+    expect(find.text('Sun'), findsOneWidget);
+    expect(find.text('Mon'), findsOneWidget);
+    expect(find.text('Tue'), findsOneWidget);
+    expect(find.text('Wed'), findsOneWidget);
+    expect(find.text('Thu'), findsOneWidget);
+    expect(find.text('Fri'), findsOneWidget);
+    expect(find.text('Sat'), findsOneWidget);
+  });
+
+  testWidgets('test narrow weekday row', (WidgetTester tester) async {
+    await tester.pumpWidget(wrapped(
+      WeekdayRow(true, WeekdayFormat.standaloneNarrow, margin, null, format),
+    ));
+
+    // sat and sun
+    expect(find.text('S'), findsNWidgets(2));
+    // thurs and tues
+    expect(find.text('T'), findsNWidgets(2));
+
+    expect(find.text('M'), findsOneWidget);
+    expect(find.text('W'), findsOneWidget);
+    expect(find.text('F'), findsOneWidget);
+  });
+
+  testWidgets('test standalone weekday row', (WidgetTester tester) async {
+    await tester.pumpWidget(wrapped(
+      WeekdayRow(true, WeekdayFormat.standalone, margin, null, format),
+    ));
+
+    expect(find.text('Sunday'), findsOneWidget);
+    expect(find.text('Monday'), findsOneWidget);
+    expect(find.text('Tuesday'), findsOneWidget);
+    expect(find.text('Wednesday'), findsOneWidget);
+    expect(find.text('Thursday'), findsOneWidget);
+    expect(find.text('Friday'), findsOneWidget);
+    expect(find.text('Saturday'), findsOneWidget);
+  });
+
+  testWidgets('test standalone short weekday row', (WidgetTester tester) async {
+    await tester.pumpWidget(wrapped(
+      WeekdayRow(true, WeekdayFormat.standaloneShort, margin, null, format),
+    ));
 
     expect(find.text('Sun'), findsOneWidget);
     expect(find.text('Mon'), findsOneWidget);
@@ -29,6 +69,6 @@ void main() {
 }
 
 Widget wrapped(Widget widget) => Directionality(
-  textDirection: TextDirection.ltr,
-  child: widget,
-);
+      textDirection: TextDirection.ltr,
+      child: widget,
+    );
