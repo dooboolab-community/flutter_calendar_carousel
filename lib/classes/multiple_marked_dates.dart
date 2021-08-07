dd1fc9bea5c167b9894dfd35270a0fd02aed0b66
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-08-07 15:00:40 +0200

    Multiple days selection using addRange method (#285)
    * Added marked date class
    
    * Added multiple_marked_dates class
    
    * Renamed market_date to marked date
    
    * Added comments
    
    * Remover border color from marked date
    
    * Users can select day range using addRange
    
    Co-authored-by: takunda <madechangu.takunda2gmail.com>

--- lib/classes/multiple_marked_dates.dart
+++ lib/classes/multiple_marked_dates.dart
@@ -11,6 +11,49 @@ class MultipleMarkedDates{
       markedDates.add(markedDate);
   }
 
+  void addRange(MarkedDate markedDate, {int plus = 0, int minus = 0}){
+    this.add(markedDate);
+    
+    
+
+    if (plus > 0){
+      int start = 1;
+      MarkedDate newAddMarkedDate;
+
+      while (start <= plus){
+
+          newAddMarkedDate = new MarkedDate(
+              color: markedDate.color, 
+              date: markedDate.date.add(Duration(days: start)),
+              textStyle: markedDate.textStyle,
+          );
+
+          this.add(newAddMarkedDate);
+
+          start += 1;
+      }
+    }
+
+    if (minus > 0){
+      int start = 1;
+      MarkedDate newSubMarkedDate;
+
+      while (start <= minus){
+
+          newSubMarkedDate = new MarkedDate(
+              color: markedDate.color, 
+              date: markedDate.date.subtract(Duration(days: start)),
+              textStyle: markedDate.textStyle,
+          );
+        
+          this.add(newSubMarkedDate);
+
+          start += 1;
+      }
+    }
+  }
+
+
   void addAll(List<MarkedDate> markedDates){
       this.markedDates.addAll(markedDates);
   }
